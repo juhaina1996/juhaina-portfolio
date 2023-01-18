a4e9bc928f7a6e393d85aa3913b9102e8961eadc
Juhaina Mufassir <110373605+juhaina1996@users.noreply.github.com>
2023-01-18 21:26:13 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-import Home from "../src/components/Home/Home";
-export default function App() {
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../src/components/Pages/home";
+import About from "../src/components/Pages/about";
+
+function App() {
   return (
-    <main>
-      <Home />
-    </main>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
